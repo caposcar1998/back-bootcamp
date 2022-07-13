085dd4a8f06a40015936fd67175ad7c22ced9404
alamrios <alam.rios>
2022-07-13 15:18:41 -0500

    wip

--- services/user_test.go
+++ services/user_test.go
@@ -5,7 +5,6 @@ import (
 	"backbootcamp/repositories"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-	"reflect"
 	"testing"
 )
 
@@ -146,131 +145,42 @@ func Test_InsertUserWhenIncorrectPasswordFormatShouldError(t *testing.T) {
 
 // TODO: user password without cypher should error
 
-func TestUserService_GetAllUsers(t *testing.T) {
-	type fields struct {
-		repository IUserRepository
+func Test_GetAllUsersShouldSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	r := repositories.NewMockIUserRepository(ctrl)
+	expected := []models.User{
+		{
+			Id:        1,
+			FirstName: "Alam Yael",
+			LastName:  "Ríos Altamirano",
+			Email:     "alamriosx@gmail.com",
+			Username:  "alamriosx",
+			Password:  "undecodedpassword",
+		},
 	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    []models.User
-		wantErr bool
-	}{
-		// success case
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &UserService{
-				repository: tt.fields.repository,
-			}
-			got, err := s.GetAllUsers()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetAllUsers() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetAllUsers() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	s := NewUserService(r)
+	r.EXPECT().
+		GetAllUsers().
+		Return(expected, nil)
+
+	got, err := s.GetAllUsers()
+	assert.Equal(t, got, expected)
+	assert.NoError(t, err)
 }
 
-func TestUserService_GetUser(t *testing.T) {
-	type fields struct {
-		repository IUserRepository
-	}
-	type args struct {
-		idUser int
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *models.User
-		wantErr bool
-	}{
-		// read with existing id should success
-		// read with no-existing id should error
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &UserService{
-				repository: tt.fields.repository,
-			}
-			got, err := s.GetUser(tt.args.idUser)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetUser() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetUser() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// TODO: single read
+// read with existing id should success
+// read with no-existing id should error
 
-func TestUserService_UpdateUser(t *testing.T) {
-	type fields struct {
-		repository IUserRepository
-	}
-	type args struct {
-		idUser int
-		user   *models.User
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *models.User
-		wantErr bool
-	}{
-		// user with complete info should success
-		// user with incomplete info should error
-		// user with no-info should error
-		// user with incorrect password format should error
-		// user password without cypher should error
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &UserService{
-				repository: tt.fields.repository,
-			}
-			got, err := s.UpdateUser(tt.args.idUser, tt.args.user)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UpdateUser() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UpdateUser() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// TODO: update user
+// user with complete info should success
+// user with incomplete info should error
+// user with no-info should error
+// user with incorrect password format should error
+// user password without cypher should error
 
-func TestUserService_DeleteUser(t *testing.T) {
-	type fields struct {
-		repository IUserRepository
-	}
-	type args struct {
-		idUser int
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// delete with existing id should success
-		// delete with no-existing id should error
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &UserService{
-				repository: tt.fields.repository,
-			}
-			if err := s.DeleteUser(tt.args.idUser); (err != nil) != tt.wantErr {
-				t.Errorf("DeleteUser() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
+// TODO: delete user
+// delete with existing id should success
+// delete with no-existing id should error
