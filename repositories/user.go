d4917142922dfa8525199e9ed15ef274ddf6a4e0
alamrios <alam.rios>
2022-07-14 12:33:03 -0500

    login logic

--- repositories/user.go
+++ repositories/user.go
@@ -15,14 +15,42 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 	}
 }
 
+func (r *UserRepository) Login(email, password string) (*models.User, error) {
+	var user *models.User
+
+	rows, err := r.db.Query("SELECT * FROM user u WHERE u.email=? AND u.password=?", email, password)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		aux := new(models.User)
+		err := rows.Scan(&aux.Id, &aux.FirstName, &aux.LastName,
+			&aux.Email, &aux.Password)
+		if err != nil {
+			return nil, err
+		}
+		user = aux
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
 func (r *UserRepository) AddUser(u *models.User) (*models.User, error) {
-	stmtIns, err := r.db.Prepare("INSERT INTO user(firstname, lastname, username, email, password) VALUES(?, ?, ?, ?, ?)")
+	stmtIns, err := r.db.Prepare("INSERT INTO user(firstname, lastname, email, password) VALUES(?, ?, ?, ?, ?)")
 	if err != nil {
 		return nil, err
 	}
 	defer stmtIns.Close()
 
-	res, err := stmtIns.Exec(u.FirstName, u.LastName, u.Username, u.Email, u.Password)
+	res, err := stmtIns.Exec(u.FirstName, u.LastName, u.Email, u.Password)
 	if err != nil {
 		return nil, err
 	}
@@ -49,7 +77,7 @@ func (r *UserRepository) GetUserById(idUser int) (*models.User, error) {
 	for rows.Next() {
 		aux := new(models.User)
 		err := rows.Scan(&aux.Id, &aux.FirstName, &aux.LastName,
-			&aux.Username, &aux.Email, &aux.Password)
+			&aux.Email, &aux.Password)
 		if err != nil {
 			return nil, err
 		}
@@ -77,7 +105,7 @@ func (r *UserRepository) GetAllUsers() ([]models.User, error) {
 	for rows.Next() {
 		aux := new(models.User)
 		err := rows.Scan(&aux.Id, &aux.FirstName, &aux.LastName,
-			&aux.Username, &aux.Email, &aux.Password)
+			&aux.Email, &aux.Password)
 		if err != nil {
 			return nil, err
 		}
@@ -94,13 +122,13 @@ func (r *UserRepository) GetAllUsers() ([]models.User, error) {
 }
 
 func (r *UserRepository) UpdateUser(idUser int, u *models.User) (*models.User, error) {
-	stmtIns, err := r.db.Prepare("UPDATE user SET firstname=?, lastname=?, username=?, email=?, password=? WHERE id=?")
+	stmtIns, err := r.db.Prepare("UPDATE user SET firstname=?, lastname=?, email=?, password=? WHERE id=?")
 	if err != nil {
 		return nil, err
 	}
 	defer stmtIns.Close()
 
-	_, err = stmtIns.Exec(u.FirstName, u.LastName, u.Username, u.Email, u.Password, idUser)
+	_, err = stmtIns.Exec(u.FirstName, u.LastName, u.Email, u.Password, idUser)
 	if err != nil {
 		return nil, err
 	}
