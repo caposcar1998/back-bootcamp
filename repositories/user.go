dff1b3d70c734998aeb40285c11044353e12eb2e
alamrios <alam.rios>
2022-07-14 09:54:26 -0500

    user repository mysql wip

--- repositories/user.go
+++ repositories/user.go
@@ -1,13 +1,124 @@
 package repositories
 
-import "database/sql"
+import (
+	"backbootcamp/models"
+	"database/sql"
+)
 
 type UserRepository struct {
-	dbClient *sql.DB
+	db *sql.DB
 }
 
-func NewUserRepository(dbClient *sql.DB) *UserRepository {
+func NewUserRepository(db *sql.DB) *UserRepository {
 	return &UserRepository{
-		dbClient: dbClient,
+		db: db,
 	}
+}
+
+func (r *UserRepository) AddUser(u *models.User) (*models.User, error) {
+	stmtIns, err := r.db.Prepare("INSERT INTO user(firstname, lastname, username, email, password) VALUES(?, ?, ?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+	defer stmtIns.Close()
+
+	res, err := stmtIns.Exec(u.FirstName, u.LastName, u.Username, u.Email, u.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	idUser, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	u.Id = int(idUser)
+	return u, nil
+}
+
+func (r *UserRepository) GetUserById(idUser int) (*models.User, error) {
+	var user *models.User
+
+	rows, err := r.db.Query("SELECT * FROM user u WHERE u.id=?", idUser)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		aux := new(models.User)
+		err := rows.Scan(&aux.Id, &aux.FirstName, &aux.LastName,
+			&aux.Username, &aux.Email, &aux.Password)
+		if err != nil {
+			return nil, err
+		}
+		user = aux
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *UserRepository) GetAllUsers() ([]models.User, error) {
+	users := make([]models.User, 0)
+
+	rows, err := r.db.Query("SELECT * FROM user u")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		aux := new(models.User)
+		err := rows.Scan(&aux.Id, &aux.FirstName, &aux.LastName,
+			&aux.Username, &aux.Email, &aux.Password)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, *aux)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (r *UserRepository) UpdateUser(idUser int, u *models.User) (*models.User, error) {
+	stmtIns, err := r.db.Prepare("UPDATE user SET firstname=?, lastname=?, username=?, email=?, password=? WHERE id=?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmtIns.Close()
+
+	_, err = stmtIns.Exec(u.FirstName, u.LastName, u.Username, u.Email, u.Password, idUser)
+	if err != nil {
+		return nil, err
+	}
+	u.Id = idUser
+	return u, nil
+}
+
+func (r *UserRepository) DeleteUser(idUser int) error {
+	stmtIns, err := r.db.Prepare("DELETE FROM user WHERE id=?")
+	if err != nil {
+		return err
+	}
+	defer stmtIns.Close()
+
+	_, err = stmtIns.Exec(idUser)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
